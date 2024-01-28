5a90afca161dc4222d8cfd7e2f80b5c631cd55d3
Nishant Mehta <nishme0708@gmail.com>
2024-01-28 23:00:33 +0530

    Add goals code

--- Form.js
+++ Form.js
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
 
 const Form = () => {
+  const [goalText, setGoalText] = useState('');
+  const [goals, setGoals] = useState([]);
+  const inputHandler = (enteredText) => {
+    setGoalText(enteredText);
+  };
+  const addGoal = () => {
+    setGoals([...goals, goalText]);
+    setGoalText('');
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder='Your goals'
           style={styles.textInput}
+          value={goalText}
+          onChangeText={inputHandler}
         ></TextInput>
-        <Button title='Add goals'></Button>
+        <Button title='Add goals' onPress={addGoal}></Button>
       </View>
-      <View>
+      <View style={styles.listContainer}>
         <Text>List of goals...</Text>
+        {goals.map((goal, index) => (
+          <View style={styles.goal} key={index}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -32,17 +49,35 @@ const styles = StyleSheet.create({
   },
   appContainer: {
     padding: 50,
+    flex: 1,
   },
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    flex: 1,
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  listContainer: {
+    flex: 5,
+  },
+  goal: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    padding: 8,
+  },
+  goalText: {
+    color: 'white',
   },
 });
 
