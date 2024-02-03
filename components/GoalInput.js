cb3a2ae8952473c06a03cbe78ac26a442d80f9c7
Nishant Mehta <nishme0708@gmail.com>
2024-02-04 03:54:22 +0530

    Update goal input

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,26 +1,51 @@
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-const GoalInput = ({ addGoal }) => {
+import {
+  Button,
+  Modal,
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+} from 'react-native';
+const GoalInput = ({ addGoal, visible, closeModal }) => {
   const [goalText, setGoalText] = useState('');
   const inputHandler = (enteredText) => {
     setGoalText(enteredText);
   };
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder='Your goals'
-        style={styles.textInput}
-        value={goalText}
-        onChangeText={inputHandler}
-      ></TextInput>
-      <Button
-        title='Add goals'
-        onPress={() => {
-          addGoal(goalText);
-          setGoalText('');
-        }}
-      ></Button>
-    </View>
+    <Modal visible={visible} animationType='slide'>
+      <View style={styles.inputContainer}>
+        <Image
+          source={require('../assets/images/goal.png')}
+          style={styles.image}
+        ></Image>
+        <TextInput
+          placeholder='Your goals'
+          style={styles.textInput}
+          value={goalText}
+          onChangeText={inputHandler}
+        ></TextInput>
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title='Add goals'
+              onPress={() => {
+                addGoal(goalText);
+                setGoalText('');
+              }}
+              color='#b180f0'
+            ></Button>
+          </View>
+          <View style={styles.button}>
+            <Button
+              title='Cancel'
+              onPress={closeModal}
+              color='#f31282'
+            ></Button>
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
@@ -33,20 +58,35 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
     flex: 1,
+    padding: 16,
+    backgroundColor: '#311b6b',
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    width: '100%',
     marginRight: 8,
-    padding: 8,
+    padding: 16,
+    color: '#120438',
+    borderRadius: 6,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: '30%',
+    marginHorizontal: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
 });
 
